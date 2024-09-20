a0726b70a714601a04435275cc41e195b0bb8e33
Igwe Ugochukwu Michael Sylvester <elinteerie@gmail.com>
2024-09-20 23:42:41 +0100

    change password router

--- todoapp/routers/users.py
+++ todoapp/routers/users.py
@@ -5,14 +5,14 @@ from database import get_db
 from sqlalchemy.orm import Session
 from .auth import get_current_user
 from models import User
-from auth import bcrypt_context
+from passlib.context import CryptContext
 
 
 router = APIRouter(prefix='/user',tags=['Users'])
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
-
+bcrypt_context = CryptContext(schemes=['argon2', 'bcrypt'], default='argon2', deprecated='auto')
 
 class UserResponse(BaseModel):
     email: str 
@@ -27,7 +27,8 @@ class UserWithMessageResponse(BaseModel):
     user: UserResponse
 
 class PasswordChangeRequest(BaseModel):
-    new_password: str
+    password: str = Field(min_length=6)
+    new_password: str = Field(min_length=6)
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=UserWithMessageResponse)
 async def get_user(user: user_dependency, db: db_dependency) -> UserResponse | None:
@@ -44,7 +45,20 @@ async def get_user(user: user_dependency, db: db_dependency) -> UserResponse | N
 
 
 @router.patch('/change-password', status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(db: db_dependency, password: PasswordChangeRequest):
+async def change_password(user: user_dependency,db: db_dependency, password: PasswordChangeRequest):
+    if not user:
+        raise HTTPException(status_code=401, detail="Not Authenticated")
+    
+    user_model = db.query(User).filter(User.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(password.password, user_model.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect Password")
+    
+    user_model.hashed_password =bcrypt_context.hash(password.new_password)
+    db.add(user_model)
+    db.commit()
+
+
 
 
     
